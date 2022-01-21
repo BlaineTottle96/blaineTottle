db537eda6e4922b0ed4b5a0155022046ffdf04cc
Blaine Tottle <78740549+BlaineTottle96@users.noreply.github.com>
2022-01-21 11:14:19 +0000

    config

--- project2/libs/php/config.php
+++ project2/libs/php/config.php
@@ -3,23 +3,15 @@
 
 	// connection details for MySQL database
 
-	// $cd_host = "178.128.32.228";
-	// $cd_port = 3306;
-	// $cd_socket = "";
-
-	$cd_host = "127.0.0.1";
+	$cd_host = "178.128.32.228";
 	$cd_port = 3306;
 	$cd_socket = "";
 
 	// database name, username and password
 
-	// $cd_dbname = "blainext_CompanyDirectory";
-	// $cd_user = "blainext_blainext";
-	// $cd_password = "9}RG33GGH!r9";
-
-	$cd_dbname = "companydirectory";
-	$cd_user = "root";
-	$cd_password = "";
+	$cd_dbname = "blainext_CompanyDirectory";
+	$cd_user = "blainext_blainext";
+	$cd_password = "9}RG33GGH!r9";
 
 	// blainext_CompanyDirectory
 	// blainext_blainext
